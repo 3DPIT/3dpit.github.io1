d513a51858235ced2af253d89579097cf28f2428
3dpit <oksos0817@naver.com>
2022-04-19 23:10:30 +0900

    no message

--- my-blog-starter/gatsby-meta-config.js
+++ my-blog-starter/gatsby-meta-config.js
@@ -2,11 +2,14 @@ module.exports = {
   title: `3DPIT Study Blog`,
   description: `Blog posted about ...`,
   author: `[KyeongMinPark]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  introduction: `Docker, C++, C#, Java, Golang으로 개발
+  모니터링운영 및 개발, 자원수집기 Beat & Exporter 개발
+  Gitlab Runner CI/CD & Hugo 연동과 테스트코드및 등을 공부와 개발중
+  ORM, TDD, BDD, DDD, DesignPattern, WebAssembly Studying`,
+  siteUrl: `https://3dpit.github.io`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    github: `https://3dpit.github.io`, // Your GitHub account
     medium: ``, // Your Medium account
     facebook: ``, // Your Facebook account
     linkedin: ``, // Your LinkedIn account
@@ -16,13 +19,13 @@ module.exports = {
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: '3dpit/gatsby-starter-bee', // Your repository for archive comment
+    utterances: '3dpit/3dpit.github.io', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: '',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
